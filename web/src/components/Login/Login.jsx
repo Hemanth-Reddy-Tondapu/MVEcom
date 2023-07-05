670a0f7ddfffbf30bab4f9865e37804f436471f1
Hemanth Reddy T <hemant@shazab.in>
2023-07-05 14:41:48 +0530

    Login component

--- web/src/components/Login/Login.jsx
+++ web/src/components/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./login.css";
-import { AiOutlineEyeInvisible } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import styles from "../../styles/styles";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -39,26 +41,70 @@ const Login = () => {
                 className="block text-sm font-medium text-gray-700"
               >
                 Password
-                <div className="mt-1">
-                  <input
-                    type="password"
-                    name="password"
-                    autoComplete="current-password"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="emailpassword"
-                  />
-                  <AiOutlineEyeInvisible
-                    className="icon-eye-invisible"
+              </label>
+              <div className="mt-1 relative">
+                <input
+                  type={visible ? "text" : "password"}
+                  name="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="emailpassword"
+                />
+                {visible ? (
+                  <AiOutlineEye
+                    className="icon-eye"
                     size={25}
                     onClick={() => setVisible(false)}
                   />
-                </div>
-              </label>
+                ) : (
+                  <AiOutlineEyeInvisible
+                    className="icon-eye"
+                    size={25}
+                    onClick={() => setVisible(true)}
+                  />
+                )}
+              </div>
+            </div>
+            <div className={`${styles.noramlFlex} justify-between`}>
+              <div className={`${styles.noramlFlex}`}>
+                <input
+                  type="checkbox"
+                  name="remember me"
+                  id="remember-me"
+                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  {" "}
+                  Remember Me
+                </label>
+              </div>
+              <div className="text-sm">
+                <a
+                  href="forgot-password"
+                  className="font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Forgot Your password?
+                </a>
+              </div>
             </div>
             <div>
-              <button className="button">Submit</button>
+              <button
+                type="submit"
+                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+              >
+                Submit
+              </button>
+            </div>
+            <div className={`${styles.noramlFlex} w-full`}>
+              <h4>Not have any account?</h4>
+              <Link to="/sign-up" className="text-blue-600 pl-2">
+                Sign Up
+              </Link>
             </div>
           </form>
         </div>
